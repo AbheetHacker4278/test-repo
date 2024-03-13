8d108d7ea55e92af79dc1cb32b4bc32044d88efd
AbheetHacker4278 <114290748+AbheetHacker4278@users.noreply.github.com>
2024-03-13 19:39:05 +0530

    DOM PRACTICE AND REACT INSTALL

--- DOM_REVISION/script.js
+++ DOM_REVISION/script.js
@@ -29,19 +29,37 @@
 
 
 
-let btn = document.querySelector('.btn');
-let display = document.querySelector('.display');
-let input = document.querySelector('.inpt');
+// let btn = document.querySelector('.btn');
+// let display = document.querySelector('.display');
+// let input = document.querySelector('.inpt');
+// let idcount = 1;
+
+// function fn(){
+//         let newDiv = document.createElement('div');
+//     let newbtn = document.createElement('button');
+//     display.setAttribute('id', idcount++);
+//     console.log(display);
+//     newDiv.textContent = input.value;
+//     newbtn.textContent = "Mark"
+//     newbtn.addEventListener('click', ()=>{
+//         newbtn.innerHTML = "Done"
+//     })
+//     display.append(newDiv , newbtn);
+// }
+// btn.addEventListener('click', fn);
+
+
+
+
+
 let idcount = 1;
-btn.addEventListener('click', () => {
-    let newDiv = document.createElement('div');
-    let newbtn = document.createElement('button');
-    display.setAttribute('id', idcount++);
-    console.log(display);
-    newDiv.textContent = input.value;
-    newbtn.textContent = "Mark"
-    newbtn.addEventListener('click', ()=>{
-        newbtn.innerHTML = "Done"
-    })
-    display.append(newDiv , newbtn);
+var data = {};
+document.querySelector('.btn').addEventListener('click', async () => {
+    let fth = await fetch('https://fakestoreapi.com/products/1');
+    let foramta = await fth.json();
+    if (eval(document.querySelector('.inpt').value) == 7) {
+        document.querySelector('.display').innerHTML = JSON.stringify(foramta);
+    } else {
+        document.querySelector('.display').innerHTML = "NOT";
+    }
 });
